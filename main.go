8fb315b8a8bb6b6eecae0e5fc2ca98ede2328ebc
Sarvesh-Ranjan-9065 <roysarvesh5220@gmail.com>
2026-03-08 17:24:25 +0530

    done with getMovies func and deleteMovie fun

--- main.go
+++ main.go
@@ -22,6 +22,24 @@ type Director struct{
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+
 func main(){
 	r := mux.NewRouter()
 	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{firstname: "John", lastname: "Doe"}})
